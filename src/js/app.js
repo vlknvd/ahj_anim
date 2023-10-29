a3517ba4c7c5be4499e3af91f4c7b06de963ff95
vlknvd <116744722+vlknvd@users.noreply.github.com>
2023-10-29 13:22:47 +0300

    Delete src directory

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.btn_collapse');
-const container = document.querySelector('.collapse_container');
-
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  container.classList.toggle('collapse_container');
-  container.classList.toggle('show');
-});
